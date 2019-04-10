427323ae9a8276dd1757be64102c0f900ecdf1bf
Brian Foo <brian@youaremyjoy.org>
2019-04-10 16:04:29 -0400

    add subjects to data

--- csv_to_json.py
+++ csv_to_json.py
@@ -9,14 +9,17 @@ from pprint import pprint
 import sys
 
 import lib.io_utils as io
+from lib.math_utils import *
 from lib.utils import *
 
 # input
 parser = argparse.ArgumentParser()
 parser.add_argument('-in', dest="INPUT_FILE", default="data/photographic_images.csv", help="File with metadata")
+parser.add_argument('-sub', dest="SUBJECTS_FILE", default="data/photographic_subjects.csv", help="File with subject data")
 parser.add_argument('-image', dest="IMAGE_FILES", default="images/photographic_thumbnails/*.jpg", help="Input file pattern")
 parser.add_argument('-grid', dest="GRID_FILE", default="data/photographic_grid.csv", help="File with grid data")
 parser.add_argument('-gsize', dest="GRID_SIZE", default="114x116", help="Grid size in cols x rows")
+parser.add_argument('-msub', dest="MAX_SUBJECTS", default=64, type=int, help="Max number of subjects (includes 'other')")
 parser.add_argument('-out', dest="OUTPUT_FILE", default="data/photographic_images.json", help="File for output")
 a = parser.parse_args()
 
@@ -29,14 +32,31 @@ io.makeDirectories(a.OUTPUT_FILE)
 
 # retrieve data
 fieldNames, data = io.readCsv(a.INPUT_FILE)
+dataCount = len(data)
+_, subjectData = io.readCsv(a.SUBJECTS_FILE)
 grid = np.loadtxt(a.GRID_FILE, delimiter=",")
 imageFiles = glob.glob(a.IMAGE_FILES)
 imageFiles = sorted(imageFiles)
 fileCount = len(imageFiles)
 print("Loaded %s files" % fileCount)
 
+# process subject data
+subjectData = groupList(subjectData, "subject", sort=True)
+subjectCount = len(subjectData)
+mainSubjects = subjectData[:a.MAX_SUBJECTS] if subjectCount > a.MAX_SUBJECTS else subjectData
+subjectLabels = [s["subject"] for s in mainSubjects]
+
+print("Matching subjects...")
 for i, d in enumerate(data):
     data[i]["years"] = [y for y in parseYears(d["date"]) if YEAR_RANGE[0] <= y <= YEAR_RANGE[1]]
+    subjects = []
+    for j, s in enumerate(mainSubjects):
+        for item in s["items"]:
+            if item["id"] == d["id"]:
+                subjects.append(j)
+                break
+    data[i]["subjects"] = subjects
+    printProgress(i+1, dataCount)
 
 if len(data) <= 0:
     print("No data found")
@@ -72,6 +92,7 @@ ids = ["" for i in range(gridW*gridH)]
 filenames = ["" for i in range(gridW*gridH)]
 titles = ["" for i in range(gridW*gridH)]
 years = [[] for i in range(gridW*gridH)]
+subjects = [[] for i in range(gridW*gridH)]
 
 for fileIndex, dataIndex in enumerate(matchedEntries):
     entry = data[dataIndex]
@@ -81,6 +102,7 @@ for fileIndex, dataIndex in enumerate(matchedEntries):
     filenames[gridIndex] = entry["imageUrl"].split("/")[-1]
     titles[gridIndex] = entry["title"]
     years[gridIndex] = entry["years"]
+    subjects[gridIndex] = entry["subjects"]
 
 jsonData = {
     "itemBaseUrl": itemBaseUrl,
@@ -88,7 +110,9 @@ jsonData = {
     "ids": ids,
     "filenames": filenames,
     "titles": titles,
-    "years": years
+    "years": years,
+    "subjects": subjects,
+    "subjectMeta": [[s["subject"], s["count"]] for s in mainSubjects]
 }
 
 with open(a.OUTPUT_FILE, 'w') as f:
--- lib/math_utils.py
+++ lib/math_utils.py
@@ -1,3 +1,22 @@
+import itertools
+from operator import itemgetter
+from pprint import pprint
+
+def groupList(arr, groupBy, sort=False, desc=True):
+    groups = []
+    arr = sorted(arr, key=itemgetter(groupBy))
+    for key, items in itertools.groupby(arr, key=itemgetter(groupBy)):
+        group = {}
+        litems = list(items)
+        count = len(litems)
+        group[groupBy] = key
+        group["items"] = litems
+        group["count"] = count
+        groups.append(group)
+    if sort:
+        reversed = desc
+        groups = sorted(groups, key=lambda k: k["count"], reverse=reversed)
+    return groups
 
 def lim(value, ab=(0, 1)):
     a, b = ab
